42741bae3d11f42621bbc9876a0fb67039c4c2b0
Janeé Mays <janee.elise@gmail.com>
2021-02-23 21:57:52 -0500

    begin building a for loop for displaying table but upon review with TAs and instructor abandoned idea for creation of divs in HTML

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,24 @@
 // select the existing DOM element
-$('.container');
+
 
 // handle displaying the day of the week, month, and date
-    var today = moment()
-    $('#currentDay').text(today.format('dddd, MMMM Do'));
+var today = moment()
+$('#currentDay').text(today.format('dddd, MMMM Do'));
 
+
+var container = $('.container')
+// var table = $('<table>')
+var ul = $('<ul>')
+container.append(ul);
+
+
+// for loop that display's the 9-5 calendar day
+for (i = 9; i < 18; i++) {
+    var tr= $('<tr>');
+    var td=$('<td>');
+    td.text(i)
+    tr.append(td)
+ul.append(tr)
+}
+// ^Needs = moment().format('h:mm a');
 
