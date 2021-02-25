9a929aec22aed4c4c28b325a43510672be1ba139
Janeé Mays <janee.elise@gmail.com>
2021-02-25 11:28:26 -0500

    added description of process to README file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,15 @@
 // select the existing DOM element
-
+var saveBtn = $('#saveBtn');
+var textInput = $('.description');
 
 // handle displaying the day of the week, month, and date
 var today = moment()
 $('#currentDay').text(today.format('dddd, MMMM Do'));
 
+function textInput(){
+    console.log()
+}
 
-// var container = $('.container')
-// // var table = $('<table>')
-// var ul = $('<ul>')
-// container.append(ul);
+saveBtn.on('click', function() { textInput() });
 
-
-// // for loop that display's the 9-5 calendar day
-// for (i = 9; i < 18; i++) {
-//     var tr= $('<tr>');
-//     var td=$('<td>');
-//     td.text(i)
-//     tr.append(td)
-// ul.append(tr)
-// }
-// // ^Needs = moment().format('h:mm a');
 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # professional-work-day-scheduler
 A simple calendar application that allows a user to save events for each hour of the day by modifying starter code
+
+# Description
+
+The first element I noticed was missing was the date in the header. I used a variable and an id to generate the date with moment.js. After formatting the date to match the supplied animation I moved on to the hour blocks. 
+
+Initially I thought the best way to organize the scheduler was a table in javascript with a for loop. I ultimately abandoned this idea after understanding that the scheduler would be static. Instead, I built the application within HTML. 
