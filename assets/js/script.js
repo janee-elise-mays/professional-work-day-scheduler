34a7a7e5af36ae1a200062e9293b57de5a798774
Janeé Mays <janee.elise@gmail.com>
2021-02-27 16:06:17 -0500

    unable to save text area data (after refresh) as well as unable to change text area color based on local time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@
 var task9 = localStorage.getItem('9')
 // set the 9 textarea to task9
 
+
+setInterval(function(){
+    console.log(moment().format('H'))
+},1000)
+
 const currentTime = moment();
 const hourAhead = moment().add(1, 'hour');
 const hourBehind = moment().add(-1,'hour');
+
 if (hourAhead.hour() > currentTime.hour()) {
     // add the future class
    hour =  $('.future');
@@ -25,7 +31,7 @@ $(document).ready(function() {
     var textInput = $('.description');
 
     $('.saveBtn').each(function() {
-        var hour = $(this).parent().attr('hour');
+        var hour = $(this).parent().attr('data-hour');
         // grab the localstorage
         // set the texarea
         // grab the current time
@@ -33,30 +39,17 @@ $(document).ready(function() {
         // compare them with if statements
     })
 
-    saveBtn.click(function() {
+    saveBtn.click(function(e) {
+        localStorage.setItem('user-input', textInput.value)
+        e.preventDefault()
+        localStorage.getItem(textInput.value)
+    })
         var hour = $(this).parent().attr('hour');
         var task = $(this).prev().val();
         console.log(hour, task)
         localStorage.setItem(hour, task);
     });
 
-    setInterval(function(){
-        console.log(moment().format('H'))
-    },1000)
 
-    const currentTime = moment();
-    const hourAhead = moment().add(1, 'hour');
-    const hourBehind = moment().add(-1,'hour');
-    
-    if (hourAhead.hour() > currentTime.hour()) {
-        // add the future class
-       hour =  $('.future');
-    }
-    else if (hourAhead.hour() < currentTime.hour()) {
-        hour = $('.past');
-    }
-    else {
-       hour = $('.present');
-    }
 });
 
